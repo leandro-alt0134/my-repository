6d1a81e1161026746e775fe374b7ab79c77e3f52
leandro-anc <leandroapnc@gmail.com>
2023-02-26 18:51:57 -0300

    Primeiro COMMIT

---
+++ src/main/java/com/example/projeto3x/Projeto3xApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto3x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto3xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto3xApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto3x/Projeto3xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto3x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto3xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
